537993a1388f3ef7829ab62219d85800ed036771
anu5priya7 <anu5priya7@gmail.com>
2023-10-10 22:09:28 +0530

    Temperature App

---
+++ app/src/main/java/com/anu5priya7/temperature_converter_app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.anu5priya7.temperature_converter_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cToF, fToC;
+    private TextView result;
+    private EditText enterTemp;
+    double result0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cToF = findViewById(R.id.cToF);
+        fToC = findViewById(R.id.fToC);
+        result = findViewById(R.id.result);
+        enterTemp = findViewById(R.id.enterTemp);
+
+        cToF.setOnClickListener(view -> {
+            double temp = Double.parseDouble(enterTemp.getText().toString());
+            result0 = (temp* 1.8) + 32;
+            result.setText(String.valueOf(result0));
+        });
+
+        fToC.setOnClickListener(view -> {
+            double temp = Double.parseDouble(enterTemp.getText().toString());
+            result0 = (temp - 32) * 0.5555555556;
+            result.setText(String.valueOf(result0));
+
+        });
+    }
+}
